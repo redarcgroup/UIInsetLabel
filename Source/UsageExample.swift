042d4cca6a585d4014a6795d5c26e0af256e25be
redarcgroup <jchundevalel@redarcgroup.com>
2016-04-21 14:26:12 -0400

    Create UsageExample.swift

---
+++ Source/UsageExample.swift
@@ -0,0 +1,5 @@
+let paddedLabel: UIInsetLabel = UIInsetLabel()
+paddedLabel.paddingInsets = UIEdgeInsets(top: 7.0, left: 7.0, bottom: 7.0, right: 7.0)
+paddedLabel.font = UIFont.boldSystemFontOfSize(14.0)
+paddedLabel.text = "Some sample text"
+self.view.addSubview(paddedLabel)
